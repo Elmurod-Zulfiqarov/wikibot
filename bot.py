2cad2cb881fe8b6be8449d1b0544fdec68c5a8b7
elmurod <elmurodzulfiqarov@gmail.com>
2022-03-29 11:48:25 +0500

    v2-wikibot

--- bot.py
+++ bot.py
@@ -3,8 +3,9 @@ import wikipedia
 
 from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = '5110299425:AAEmDMMDzwPOy3_3ShGyOAx4SqpV5okw6Bs'
+API_TOKEN = 'Your Bot Token'
 wikipedia.set_lang('uz')
+ADMINS=[59083470] # your telegram id
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -14,22 +15,37 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     """
-    This handler will be called when user sends `/start` or `/help` command
+    This handler will be called when user send `/start` 
     """
-    await message.reply("Wikipedia Widiruvi Botiga Xush Kelibsiz!")
+    await message.reply(f"Salom 👋 {message.from_user.full_name}! \n"
+                         "🌱 Wikipedia Qidiruvi Botiga Xush Kelibsiz!")
+    if message.from_user.username:
+        user_n = message.from_user.username
+    else:
+        user_n = "usernami yo'q"
+    msg = f"{message.from_user.full_name} va @{user_n} foydalanuvchi Wikipedia botdan foydalandi"
+    await bot.send_message(chat_id=ADMINS[0], text=msg)
+
+@dp.message_handler(commands=['help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user send command
+    """
+    await message.reply(f"✅ Wikipedia Qidiruvi Botidan foydalanish uchun bizga maqolani mavzusini yuboring! \n"
+                         "❗️ Maqola mavzusi to'g'ri kiritilmasa yoki Wikipediada majud bo'lmasa, Siz kiritgan mavzudagi maqola topilmaganligi bo'yicha xabar yuboriladi!")
 
 
 @dp.message_handler()
 async def sendWiki(message: types.Message):
     try:
         respond = wikipedia.summary(message.text)
-        await message.answer(respond)
+        await message.reply(respond)
     except:
-        await message.answer("Bu mavzuga oid maqola topilmadi")
-
+        await message.reply("Bu mavzuga oid maqola topilmadi")
+    
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
